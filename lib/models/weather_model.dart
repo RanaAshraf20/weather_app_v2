7a3f580bcba4dae18b20ad9f48f644f8626599b6
RANA ASHRAF <ra3748797@gmail.com>
2024-11-11 01:24:07 +0200

    create weather model and its constructor as all its attributes are required

---
+++ lib/models/weather_model.dart
@@ -0,0 +1,11 @@
+class WeatherModel {
+  final String city;
+  final String time;
+  final String image;
+  final double avgTemp;
+  final double maxTemp;
+  final double minTemp;
+  final String weatherCondition;
+
+  WeatherModel({required this.city, required this.time, required this.image, required this.avgTemp, required this.maxTemp, required this.minTemp, required this.weatherCondition});
+}
