db210447678fee64fdae9176f3e52ae4772ce4e9
RANA ASHRAF <ra3748797@gmail.com>
2024-11-11 01:40:50 +0200

    create factory named constructor it is used when create a model to store data come from api it returns an object of weather model

--- lib/models/weather_model.dart
+++ lib/models/weather_model.dart
@@ -7,5 +7,24 @@ class WeatherModel {
   final double minTemp;
   final String weatherCondition;
 
-  WeatherModel({required this.city, required this.time, required this.image, required this.avgTemp, required this.maxTemp, required this.minTemp, required this.weatherCondition});
+  WeatherModel({
+    required this.city,
+    required this.time,
+    required this.image,
+    required this.avgTemp,
+    required this.maxTemp,
+    required this.minTemp,
+    required this.weatherCondition,
+  });
+  factory WeatherModel.fromJson(json) {
+    return WeatherModel(
+        city: json['location']['name'],
+        time: json['location']['localtime'],
+        image: json['forecast']['forecastday'][0]['day']['condition']['icon'],
+        avgTemp: json['forecast']['forecastday'][0]['day']['avgtemp_c'],
+        maxTemp: json['forecast']['forecastday'][0]['day']['maxtemp_c'],
+        minTemp: json['forecast']['forecastday'][0]['day']['mintemp_c'],
+        weatherCondition: json['forecast']['forecastday'][0]['day']['condition']
+            ['text']);
+  }
 }
