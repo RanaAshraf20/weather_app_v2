c2a6ac9af03f2a23b497fe39cec40e097e309f38
RANA ASHRAF <ra3748797@gmail.com>
2024-11-11 04:29:50 +0200

    create bloc builder to rebuild this part of the ui when that data came as it listen to the cubit

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app_v2/cubits/get_weather_cubit/get_current_weather_cubit.dart';
+import 'package:weather_app_v2/cubits/get_weather_cubit/get_current_weather_state.dart';
 import 'package:weather_app_v2/views/search_view.dart';
 import 'package:weather_app_v2/widgets/no_weather_body.dart';
 import 'package:weather_app_v2/widgets/weather_info_body.dart';
@@ -9,20 +12,28 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Weather'),
-        actions: [
-          IconButton(
-            onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return const SearchView();
-              }));
-            },
-            icon: const Icon(Icons.search),
-          ),
-        ],
-      ),
-      body: const WeatherInfoBody(),
-    );
+        appBar: AppBar(
+          title: const Text('Weather'),
+          actions: [
+            IconButton(
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return const SearchView();
+                }));
+              },
+              icon: const Icon(Icons.search),
+            ),
+          ],
+        ),
+        body: BlocBuilder<GetCurrentWeatherCubit, WeatherState>(
+            builder: (context, state) {
+          if (state is WeatherFailerState) {
+            return Text(state.failerMessage);
+          } else if (state is WeatherLoadedState) {
+            return const WeatherInfoBody();
+          } else {
+            return const NoWeatherBody();
+          }
+        }));
   }
 }
